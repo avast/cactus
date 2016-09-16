8d935a9baeb79b9fdcf36bba138cc94d4e24638c
kolena <kolena@avast.com>
2016-09-16 21:02:23 +0200

    CactusMacros methods encapsulation

--- macros/src/main/scala/com/avast/cactus/CactusMacros.scala
+++ macros/src/main/scala/com/avast/cactus/CactusMacros.scala
@@ -45,7 +45,7 @@ object CactusMacros {
           import scala.collection.JavaConverters._
 
           try {
-            Right(${caseClassConverter(c)(tpe, q" ${TermName("gpb")} ")})
+            Right(${GpbToCaseClass.createConverter(c)(tpe, q" ${TermName("gpb")} ")})
           } catch {
             case e: CactusException => Left(e.failure)
           }
@@ -54,82 +54,89 @@ object CactusMacros {
     }
   }
 
-  private def caseClassConverter(c: whitebox.Context)(tpe: c.universe.Type, gpb: c.Tree): c.Tree = {
-    import c.universe._
+  private object GpbToCaseClass {
 
-    if (!tpe.typeSymbol.isClass) {
-      c.abort(c.enclosingPosition, s"Provided type $tpe is not a class")
-    }
+    def createConverter(c: whitebox.Context)(tpe: c.universe.Type, gpb: c.Tree): c.Tree = {
+      import c.universe._
 
-    val classSymbol = tpe.typeSymbol.asClass
-
-    if (!classSymbol.isCaseClass) {
-      c.abort(c.enclosingPosition, s"Provided type $tpe is not a case class")
-    }
-
-    val ctor = tpe.decls.collectFirst {
-      case m: MethodSymbol if m.isPrimaryConstructor => m
-    }.get
-
-    val fields = ctor.paramLists.flatten
-
-    val params = fields.map { field =>
-      val fieldName = field.name.decodedName.toTermName
-      val returnType = field.typeSignature
-
-      val annotsTypes = field.annotations.map(_.tree.tpe.toString)
-      val annotsParams = field.annotations.map {
-        _.tree.children.tail.map { case q" $name = $value " =>
-          name.toString() -> c.eval[String](c.Expr(q"$value"))
-        }.toMap
+      if (!tpe.typeSymbol.isClass) {
+        c.abort(c.enclosingPosition, s"Provided type $tpe is not a class")
       }
 
-      val annotations = annotsTypes.zip(annotsParams).toMap
+      val classSymbol = tpe.typeSymbol.asClass
 
-      val gpbNameAnnotations = annotations.find { case (key, _) => key == classOf[GpbName].getName }
+      if (!classSymbol.isCaseClass) {
+        c.abort(c.enclosingPosition, s"Provided type $tpe is not a case class")
+      }
 
-      val nameInGpb = gpbNameAnnotations.flatMap { case (_, par) =>
-        par.get("value")
-      }.map(_.toString()).getOrElse(fieldName.toString)
+      val ctor = tpe.decls.collectFirst {
+        case m: MethodSymbol if m.isPrimaryConstructor => m
+      }.get
 
-      val upper = firstUpper(nameInGpb.toString)
-      val query = TermName(s"has$upper")
-      val getter = TermName(s"get$upper")
+      val fields = ctor.paramLists.flatten
 
-      val value = processEndType(c)(fieldName, returnType)(q"$gpb.$query", q"$gpb.$getter")
+      val params = fields.map { field =>
+        val fieldName = field.name.decodedName.toTermName
+        val returnType = field.typeSignature
 
-      c.Expr(q"$fieldName = $value")
+        val annotsTypes = field.annotations.map(_.tree.tpe.toString)
+        val annotsParams = field.annotations.map {
+          _.tree.children.tail.map { case q" $name = $value " =>
+            name.toString() -> c.eval[String](c.Expr(q"$value"))
+          }.toMap
+        }
+
+        val annotations = annotsTypes.zip(annotsParams).toMap
+
+        val gpbNameAnnotations = annotations.find { case (key, _) => key == classOf[GpbName].getName }
+
+        val nameInGpb = gpbNameAnnotations.flatMap { case (_, par) =>
+          par.get("value")
+        }.map(_.toString()).getOrElse(fieldName.toString)
+
+        val upper = firstUpper(nameInGpb.toString)
+        val query = TermName(s"has$upper")
+        val getter = TermName(s"get$upper")
+
+        val value = processEndType(c)(fieldName, returnType)(q"$gpb.$query", q"$gpb.$getter")
+
+        c.Expr(q"$fieldName = $value")
+      }
+
+      q" { ${TermName(classSymbol.name.toString)}(..$params) } "
     }
 
-    q" { ${TermName(classSymbol.name.toString)}(..$params) } "
+
+    private def processEndType(c: whitebox.Context)
+                              (name: c.universe.TermName, returnType: c.universe.Type)
+                              (query: c.universe.Tree, getter: c.universe.Tree): c.Tree = {
+      import c.universe._
+
+      val typeSymbol = returnType.typeSymbol
+      val resultType = returnType.resultType
+
+      resultType.toString match {
+        case OptPattern(t) => // Option[T]
+          val typeArg = resultType.typeArgs.head // it's an Option, so it has 1 type arg
+
+          q"CactusMacros.OptAToOptB(CactusMacros.tryToOption(Try(${processEndType(c)(name, typeArg)(query, getter)})))"
+
+        case t if typeSymbol.isClass && typeSymbol.asClass.isCaseClass => // case class
+          q" if ($query) ${createConverter(c)(returnType, q"$getter ")} else throw CactusException(MissingFieldFailure(${name.toString})) "
+
+
+        case t if typeSymbol.isClass && typeSymbol.asClass.baseClasses.map(_.name.toString).contains("TraversableLike") => // collection
+          // collections don't have the "has" method, test size instead
+          q" if ($getter.size() > 0) $getter.asScala.toList else throw CactusException(MissingFieldFailure(${name.toString})) "
+
+        case t => // plain type
+          q" if ($query) $getter else throw CactusException(MissingFieldFailure(${name.toString})) "
+      }
+    }
   }
 
+  private object CaseClassToGpb {
 
-  private def processEndType(c: whitebox.Context)
-                            (name: c.universe.TermName, returnType: c.universe.Type)
-                            (query: c.universe.Tree, getter: c.universe.Tree): c.Tree = {
-    import c.universe._
-
-    val typeSymbol = returnType.typeSymbol
-    val resultType = returnType.resultType
-
-    resultType.toString match {
-      case OptPattern(t) => // Option[T]
-        val typeArg = resultType.typeArgs.head // it's an Option, so it has 1 type arg
-
-        q"CactusMacros.OptAToOptB(CactusMacros.tryToOption(Try(${processEndType(c)(name, typeArg)(query, getter)})))"
-
-      case t if typeSymbol.isClass && typeSymbol.asClass.isCaseClass => // case class
-        q" if ($query) ${caseClassConverter(c)(returnType, q"$getter ")} else throw CactusException(MissingFieldFailure(${name.toString})) "
-
-
-      case t if typeSymbol.isClass && typeSymbol.asClass.baseClasses.map(_.name.toString).contains("TraversableLike") => // collection
-        // collections don't have the "has" method, test size instead
-        q" if ($getter.size() > 0) $getter.asScala.toList else throw CactusException(MissingFieldFailure(${name.toString})) "
-
-      case t => // plain type
-        q" if ($query) $getter else throw CactusException(MissingFieldFailure(${name.toString})) "
-    }
   }
 
   private def firstUpper(s: String): String = {
