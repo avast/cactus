54031d242e549810dcfeb677250fdc90cbff3842
kolena <kolena@avast.com>
2016-12-02 16:43:25 +0100

    uncommented CactusMacrosTest2

--- macros/src/test/scala/com/avast/cactus/CactusMacrosTest2.scala
+++ macros/src/test/scala/com/avast/cactus/CactusMacrosTest2.scala
@@ -1,27 +1,27 @@
-//package com.avast.cactus
-//
-//import com.avast.cactus.TestMessage.Data
-//import com.google.protobuf.ByteString
-//import org.scalactic.Good
-//import org.scalatest.FunSuite
-//
-//// This test exists to be sure this compiles. It's about not-needed import for internal GPB classes (like `Data2` in this case).
-//class CactusMacrosTest2 extends FunSuite {
-//
-//  // user specified converters
-//  implicit val StringToByteStringConverter: Converter[String, ByteString] = Converter((b: String) => ByteString.copyFromUtf8(b))
-//  implicit val ByteStringToStringConverter: Converter[ByteString, String] = Converter((b: ByteString) => b.toStringUtf8)
-//
-//  implicit val StringWrapperToStringConverter: Converter[StringWrapperClass, String] = Converter((b: StringWrapperClass) => b.value)
-//  implicit val StringToStringWrapperConverter: Converter[String, StringWrapperClass] = Converter((b: String) => StringWrapperClass(b))
-//
-//  test("convert case class to GPB and back") {
-//    val map = Map("first" -> "1", "second" -> "2")
-//
-//    val original = CaseClassC(StringWrapperClass("ahoj"), 9, Some(13), ByteString.EMPTY, Vector("a"), CaseClassB(0.9, "text"), Some(CaseClassB(0.9, "text")), None, Array("a", "b"), Vector(3, 6), List(), map)
-//
-//    val Good(converted) = original.asGpb[Data]
-//
-//    assertResult(Good(original))(converted.asCaseClass[CaseClassC])
-//  }
-//}
+package com.avast.cactus
+
+import com.avast.cactus.TestMessage.Data
+import com.google.protobuf.ByteString
+import org.scalactic.Good
+import org.scalatest.FunSuite
+
+// This test exists to be sure this compiles. It's about not-needed import for internal GPB classes (like `Data2` in this case).
+class CactusMacrosTest2 extends FunSuite {
+
+  // user specified converters
+  implicit val StringToByteStringConverter: Converter[String, ByteString] = Converter((b: String) => ByteString.copyFromUtf8(b))
+  implicit val ByteStringToStringConverter: Converter[ByteString, String] = Converter((b: ByteString) => b.toStringUtf8)
+
+  implicit val StringWrapperToStringConverter: Converter[StringWrapperClass, String] = Converter((b: StringWrapperClass) => b.value)
+  implicit val StringToStringWrapperConverter: Converter[String, StringWrapperClass] = Converter((b: String) => StringWrapperClass(b))
+
+  test("convert case class to GPB and back") {
+    val map = Map("first" -> "1", "second" -> "2")
+
+    val original = CaseClassC(StringWrapperClass("ahoj"), 9, Some(13), ByteString.EMPTY, Vector("a"), CaseClassB(0.9, "text"), Some(CaseClassB(0.9, "text")), None, Array("a", "b"), Vector(3, 6), List(), map)
+
+    val Good(converted) = original.asGpb[Data]
+
+    assertResult(Good(original))(converted.asCaseClass[CaseClassC])
+  }
+}
