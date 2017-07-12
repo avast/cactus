e0ec9dbbb9f25e424c33922460ba9429148d1f01
kolena <kolena@avast.com>
2017-07-12 11:17:47 +0200

    Support for Bytes library - BytesValue converters

--- macros/src/main/scala/com/avast/cactus/OptionalConverters.scala
+++ macros/src/main/scala/com/avast/cactus/OptionalConverters.scala
@@ -2,14 +2,23 @@ package com.avast.cactus
 
 import com.avast.bytes.Bytes
 import com.avast.bytes.gpb.ByteStringBytes
-import com.google.protobuf.ByteString
+import com.google.protobuf.{ByteString, BytesValue}
 
 trait OptionalConverters {
   implicit val BytesToByteStringConverter: Converter[Bytes, ByteString] = Converter {
-    case b: ByteStringBytes => ByteString.copyFrom(b.toReadOnlyByteBuffer)
+    case b: ByteStringBytes => b.underlying()
     case b: Bytes => ByteString.copyFrom(b.toReadOnlyByteBuffer)
   }
 
   implicit val ByteStringToBytesConverter: Converter[ByteString, Bytes] = Converter((b: ByteString) => ByteStringBytes.wrap(b))
+
+  // v3:
+
+  implicit val bytes2bytesValue: Converter[Bytes, BytesValue] = Converter {
+    case b: ByteStringBytes => BytesValue.newBuilder().setValue(b.underlying()).build()
+    case b: Bytes => BytesValue.newBuilder().setValue(ByteString.copyFrom(b.toReadOnlyByteBuffer)).build()
+  }
+
+  implicit val bytesValue2Bytes: Converter[BytesValue, Bytes] = Converter(v => ByteStringBytes.wrap(v.getValue))
 
 }
