4a6c9f16713e520cc812cf5cfafd9d8078ff140b
kolena <kolena@avast.com>
2017-08-02 15:44:11 +0200

    V3 and optional converters be `lazy`

--- macros/src/main/scala/com/avast/cactus/OptionalConverters.scala
+++ macros/src/main/scala/com/avast/cactus/OptionalConverters.scala
@@ -5,20 +5,20 @@ import com.avast.bytes.gpb.ByteStringBytes
 import com.google.protobuf.{ByteString, BytesValue}
 
 trait OptionalConverters {
-  implicit val BytesToByteStringConverter: Converter[Bytes, ByteString] = Converter {
+  implicit lazy val BytesToByteStringConverter: Converter[Bytes, ByteString] = Converter {
     case b: ByteStringBytes => b.underlying()
     case b: Bytes => ByteString.copyFrom(b.toReadOnlyByteBuffer)
   }
 
-  implicit val ByteStringToBytesConverter: Converter[ByteString, Bytes] = Converter((b: ByteString) => ByteStringBytes.wrap(b))
+  implicit lazy val ByteStringToBytesConverter: Converter[ByteString, Bytes] = Converter((b: ByteString) => ByteStringBytes.wrap(b))
 
   // v3:
 
-  implicit val bytes2bytesValue: Converter[Bytes, BytesValue] = Converter {
+  implicit lazy val bytes2bytesValue: Converter[Bytes, BytesValue] = Converter {
     case b: ByteStringBytes => BytesValue.newBuilder().setValue(b.underlying()).build()
     case b: Bytes => BytesValue.newBuilder().setValue(ByteString.copyFrom(b.toReadOnlyByteBuffer)).build()
   }
 
-  implicit val bytesValue2Bytes: Converter[BytesValue, Bytes] = Converter(v => ByteStringBytes.wrap(v.getValue))
+  implicit lazy val bytesValue2Bytes: Converter[BytesValue, Bytes] = Converter(v => ByteStringBytes.wrap(v.getValue))
 
 }
--- macros/src/main/scala/com/avast/cactus/v3/V3Converters.scala
+++ macros/src/main/scala/com/avast/cactus/v3/V3Converters.scala
@@ -9,43 +9,43 @@ import org.scalactic.Accumulation._
 import scala.collection.JavaConverters._
 
 trait V3Converters {
-  implicit val listValue2SeqConverter: Converter[com.google.protobuf.ListValue, Seq[ValueOneOf]] = Converter.checked { (fieldPath, listValue) =>
+  implicit lazy val listValue2SeqConverter: Converter[com.google.protobuf.ListValue, Seq[ValueOneOf]] = Converter.checked { (fieldPath, listValue) =>
     listValue.getValuesList.asScala.map(ValueOneOf.apply(fieldPath, _)).toIterable.combined.map(_.toSeq)
   }
 
-  implicit val seq2ListValueConverter: Converter[Seq[ValueOneOf], com.google.protobuf.ListValue] = Converter { values =>
+  implicit lazy val seq2ListValueConverter: Converter[Seq[ValueOneOf], com.google.protobuf.ListValue] = Converter { values =>
     ListValue.newBuilder().addAllValues(values.map(ValueOneOf.toGpbValue).asJava).build()
   }
 
-  implicit val struct2MapConverter: Converter[com.google.protobuf.Struct, Map[String, ValueOneOf]] = Converter.checked { (fieldPath, struct) =>
+  implicit lazy val struct2MapConverter: Converter[com.google.protobuf.Struct, Map[String, ValueOneOf]] = Converter.checked { (fieldPath, struct) =>
     struct.getFieldsMap.asScala.mapValues(ValueOneOf.apply(fieldPath, _)).map { case (key, value) => value.map(key -> _) }.combined.map(_.toMap)
   }
 
-  implicit val map2StructConverter: Converter[Map[String, ValueOneOf], com.google.protobuf.Struct] = Converter { m =>
+  implicit lazy val map2StructConverter: Converter[Map[String, ValueOneOf], com.google.protobuf.Struct] = Converter { m =>
     Struct.newBuilder().putAllFields(m.mapValues(ValueOneOf.toGpbValue).asJava).build()
   }
 
-  implicit val doubleValue2Double: Converter[DoubleValue, Double] = Converter(_.getValue)
-  implicit val stringValue2String: Converter[StringValue, String] = Converter(_.getValue)
-  implicit val floatValue2Float: Converter[FloatValue, Float] = Converter(_.getValue)
-  implicit val boolValue2Boolean: Converter[BoolValue, Boolean] = Converter(_.getValue)
-  implicit val int64Value2Long: Converter[Int64Value, Long] = Converter(_.getValue)
-  implicit val int32Value2Int: Converter[Int32Value, Int] = Converter(_.getValue)
-  implicit val bytesValue2ByteString: Converter[BytesValue, ByteString] = Converter(_.getValue)
-  implicit val gpbDuration2Duration: Converter[GpbDuration, Duration] = Converter { v => Duration.ofSeconds(v.getSeconds, v.getNanos) }
-  implicit val gpbTimestamp2Instant: Converter[GpbTimestamp, Instant] = Converter { v => Instant.ofEpochSecond(v.getSeconds, v.getNanos) }
+  implicit lazy val doubleValue2Double: Converter[DoubleValue, Double] = Converter(_.getValue)
+  implicit lazy val stringValue2String: Converter[StringValue, String] = Converter(_.getValue)
+  implicit lazy val floatValue2Float: Converter[FloatValue, Float] = Converter(_.getValue)
+  implicit lazy val boolValue2Boolean: Converter[BoolValue, Boolean] = Converter(_.getValue)
+  implicit lazy val int64Value2Long: Converter[Int64Value, Long] = Converter(_.getValue)
+  implicit lazy val int32Value2Int: Converter[Int32Value, Int] = Converter(_.getValue)
+  implicit lazy val bytesValue2ByteString: Converter[BytesValue, ByteString] = Converter(_.getValue)
+  implicit lazy val gpbDuration2Duration: Converter[GpbDuration, Duration] = Converter { v => Duration.ofSeconds(v.getSeconds, v.getNanos) }
+  implicit lazy val gpbTimestamp2Instant: Converter[GpbTimestamp, Instant] = Converter { v => Instant.ofEpochSecond(v.getSeconds, v.getNanos) }
 
-  implicit val double2DoubleValue: Converter[Double, DoubleValue] = Converter(DoubleValue.newBuilder().setValue(_).build())
-  implicit val string2stringValue: Converter[String, StringValue] = Converter(StringValue.newBuilder().setValue(_).build())
-  implicit val float2floatValue: Converter[Float, FloatValue] = Converter(FloatValue.newBuilder().setValue(_).build())
-  implicit val boolean2boolValue: Converter[Boolean, BoolValue] = Converter(BoolValue.newBuilder().setValue(_).build())
-  implicit val long2int64Value: Converter[Long, Int64Value] = Converter(Int64Value.newBuilder().setValue(_).build())
-  implicit val int2int32Value: Converter[Int, Int32Value] = Converter(Int32Value.newBuilder().setValue(_).build())
-  implicit val byteString2bytesValue: Converter[ByteString, BytesValue] = Converter(BytesValue.newBuilder().setValue(_).build())
-  implicit val duration2gpbDuration: Converter[Duration, GpbDuration] = Converter { d =>
+  implicit lazy val double2DoubleValue: Converter[Double, DoubleValue] = Converter(DoubleValue.newBuilder().setValue(_).build())
+  implicit lazy val string2stringValue: Converter[String, StringValue] = Converter(StringValue.newBuilder().setValue(_).build())
+  implicit lazy val float2floatValue: Converter[Float, FloatValue] = Converter(FloatValue.newBuilder().setValue(_).build())
+  implicit lazy val boolean2boolValue: Converter[Boolean, BoolValue] = Converter(BoolValue.newBuilder().setValue(_).build())
+  implicit lazy val long2int64Value: Converter[Long, Int64Value] = Converter(Int64Value.newBuilder().setValue(_).build())
+  implicit lazy val int2int32Value: Converter[Int, Int32Value] = Converter(Int32Value.newBuilder().setValue(_).build())
+  implicit lazy val byteString2bytesValue: Converter[ByteString, BytesValue] = Converter(BytesValue.newBuilder().setValue(_).build())
+  implicit lazy val duration2gpbDuration: Converter[Duration, GpbDuration] = Converter { d =>
     GpbDuration.newBuilder().setSeconds(d.getSeconds).setNanos(d.getNano).build()
   }
-  implicit val instant2gpbTimestamp: Converter[Instant, GpbTimestamp] = Converter { i =>
+  implicit lazy val instant2gpbTimestamp: Converter[Instant, GpbTimestamp] = Converter { i =>
     GpbTimestamp.newBuilder().setSeconds(i.getEpochSecond).setNanos(i.getNano).build()
   }
 }
