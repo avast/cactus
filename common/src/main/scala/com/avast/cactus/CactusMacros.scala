1e913bf1b6a597790bef2b91e57f00e62b5295f8
kolena <kolena@avast.com>
2018-01-24 18:03:49 +0100

    Use FQN for Converter class in macros

--- common/src/main/scala/com/avast/cactus/CactusMacros.scala
+++ common/src/main/scala/com/avast/cactus/CactusMacros.scala
@@ -73,7 +73,7 @@ object CactusMacros {
     val variableName = variable.symbol.asTerm.fullName.split('.').last
 
     c.Expr[CaseClass Or Every[CactusFailure]] {
-      q" implicitly[Converter[$gpbType, $caseClassType]].apply($variableName)($variable) "
+      q" implicitly[com.avast.cactus.Converter[$gpbType, $caseClassType]].apply($variableName)($variable) "
     }
   }
 
@@ -88,7 +88,7 @@ object CactusMacros {
     val variableName = variable.symbol.asTerm.fullName.split('.').last
 
     c.Expr[Gpb Or Every[CactusFailure]] {
-      q" implicitly[Converter[$caseClassType, $gpbType]].apply($variableName)($variable) "
+      q" implicitly[com.avast.cactus.Converter[$caseClassType, $gpbType]].apply($variableName)($variable) "
     }
   }
 
@@ -131,7 +131,7 @@ object CactusMacros {
 
     c.Expr[Converter[GpbClass, CaseClass]] {
       q"""
-         new Converter[$gpbType, $caseClassType] {
+         new com.avast.cactus.Converter[$gpbType, $caseClassType] {
             def apply(fieldPath: String)(gpb: $gpbType): com.avast.cactus.ResultOrErrors[$caseClassType] = $theFunction
          }
        """
@@ -177,7 +177,7 @@ object CactusMacros {
 
     c.Expr[Converter[CaseClass, GpbClass]] {
       q"""
-         new Converter[$caseClassType, $gpbType] {
+         new com.avast.cactus.Converter[$caseClassType, $gpbType] {
             def apply(fieldPath: String)(instance: $caseClassType): com.avast.cactus.ResultOrErrors[$gpbType] = $theFunction
          }
        """
@@ -1009,7 +1009,7 @@ object CactusMacros {
           println(s"Defining converter from ${from.typeSymbol} to ${to.typeSymbol}")
         }
 
-        converters += key -> q" implicit lazy val ${TermName(s"conv${converters.size}")}:Converter[$from, $to] = Converter.checked($f) "
+        converters += key -> q" implicit lazy val ${TermName(s"conv${converters.size}")}:com.avast.cactus.Converter[$from, $to] = com.avast.cactus.Converter.checked($f) "
       })
     }
 
