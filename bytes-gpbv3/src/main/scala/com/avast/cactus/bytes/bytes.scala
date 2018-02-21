48ed8b622e96c076c2dabba2092e1d56757547c0
kolena <kolena@avast.com>
2018-02-21 15:56:21 +0100

    Removed `lazy` from v3 converters

--- bytes-gpbv3/src/main/scala/com/avast/cactus/bytes/bytes.scala
+++ bytes-gpbv3/src/main/scala/com/avast/cactus/bytes/bytes.scala
@@ -5,11 +5,11 @@ import com.avast.bytes.gpb.ByteStringBytes
 import com.google.protobuf.{ByteString, BytesValue}
 
 package object bytes {
-  implicit lazy val bytes2bytesValue: Converter[Bytes, BytesValue] = Converter {
+  implicit val bytes2bytesValue: Converter[Bytes, BytesValue] = Converter {
     case b: ByteStringBytes => BytesValue.newBuilder().setValue(b.underlying()).build()
     case b: Bytes => BytesValue.newBuilder().setValue(ByteString.copyFrom(b.toReadOnlyByteBuffer)).build()
   }
 
-  implicit lazy val bytesValue2Bytes: Converter[BytesValue, Bytes] = Converter(v => ByteStringBytes.wrap(v.getValue))
+  implicit val bytesValue2Bytes: Converter[BytesValue, Bytes] = Converter(v => ByteStringBytes.wrap(v.getValue))
 
 }
--- bytes/src/main/scala/com/avast/cactus/bytes/bytes.scala
+++ bytes/src/main/scala/com/avast/cactus/bytes/bytes.scala
@@ -5,11 +5,11 @@ import com.avast.bytes.gpb.ByteStringBytes
 import com.google.protobuf.ByteString
 
 package object bytes {
-  implicit lazy val BytesToByteStringConverter: Converter[Bytes, ByteString] = Converter {
+  implicit val BytesToByteStringConverter: Converter[Bytes, ByteString] = Converter {
     case b: ByteStringBytes => b.underlying()
     case b: Bytes => ByteString.copyFrom(b.toReadOnlyByteBuffer)
   }
 
-  implicit lazy val ByteStringToBytesConverter: Converter[ByteString, Bytes] = Converter((b: ByteString) => ByteStringBytes.wrap(b))
+  implicit val ByteStringToBytesConverter: Converter[ByteString, Bytes] = Converter((b: ByteString) => ByteStringBytes.wrap(b))
 
 }
