286d5f6351a84b0ec78f301093983abdc3713039
kolena <kolena@avast.com>
2019-02-07 20:29:23 +0100

    AnyValueConverterOps

--- gpbv3/src/main/scala/com/avast/cactus/v3/v3.scala
+++ gpbv3/src/main/scala/com/avast/cactus/v3/v3.scala
@@ -8,7 +8,7 @@ import scala.reflect.macros.whitebox
 package object v3 extends CactusCommonImplicits with V3Converters {
 
   implicit class AnyValueParser(val anyValue: AnyValue) extends AnyVal {
-    def asGpb[Gpb <: Message : AnyValueConverter]: ResultOrErrors[Gpb] = macro anyValueAsGpbMethod[Gpb]
+    def asGpb[Gpb <: Message: AnyValueConverter]: ResultOrErrors[Gpb] = macro anyValueAsGpbMethod[Gpb]
     def asCaseClass[CaseClass: AnyValueConverter]: ResultOrErrors[CaseClass] = macro anyValueAsCCMethod[CaseClass]
   }
 
@@ -38,4 +38,23 @@ package object v3 extends CactusCommonImplicits with V3Converters {
     }
   }
 
+  implicit class AnyValueConverterOps[A](val conv: AnyValueConverter[A]) {
+    final def map[B](f: A => B): Converter[AnyValue, B] = new Converter[AnyValue, B] {
+      override def apply(fieldPath: String)(a: AnyValue): ResultOrErrors[B] = conv.apply(fieldPath)(a).map(f)
+    }
+
+    final def andThen[B](implicit c: Converter[A, B]): Converter[AnyValue, B] = new Converter[AnyValue, B] {
+      override def apply(fieldPath: String)(a: AnyValue): ResultOrErrors[B] = conv.apply(fieldPath)(a).flatMap(c.apply(fieldPath))
+    }
+
+    final def contraMap[AA](f: AA => AnyValue): Converter[AA, A] =
+      Converter(f).andThen(Converter.checked { (path, av) =>
+        conv.apply(path)(av)
+      })
+
+    final def compose[AA](implicit f: Converter[AA, AnyValue]): Converter[AA, A] =
+      f.andThen(Converter.checked { (path, av) =>
+        conv.apply(path)(av)
+      })
+  }
 }
